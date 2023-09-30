36754eded6f1571ba050fcd2ad37604e9f756ea3
Emerson Torres <36451877+Apemerson@users.noreply.github.com>
2023-09-30 18:40:41 -0300

    Add files via upload

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -24,12 +24,13 @@ print_banner() {
 printf "${CYAN_LIGHT}";
 
 
-printf ${CYAN_LIGHT}"░▄░█░░░▄▀▀▀▀▀▄░░░█░▄░\n";
-printf ${CYAN_LIGHT}"▄▄▀▄░░░█─▀─▀─█░░░▄▀▄▄\n";
-printf ${CYAN_LIGHT}"░░░░▀▄▒▒▒▒▒▒▒▒▒▄▀░░░░\n";
-printf ${CYAN_LIGHT}"░░░░░█────▀────█░░░░░\n";
-printf ${CYAN_LIGHT}"░░░░░█────▀────█░░░░░\n\n";
-printf ${CYAN_LIGHT}"★★★★彡ＴＯＲＲＥＳ彡★★★★n";
+printf ${CYAN_LIGHT}"          ★彡[̲V̲̲A̲̲M̲̲O̲̲S̲ ̲C̲̲O̲̲M̲̲E̲Ç̲A̲̲R̲!!!]彡★\n\n";
+printf ${CYAN_LIGHT}"          ░▄░█░░░▄▀▀▀▀▀▄░░░█░▄░\n";
+printf ${CYAN_LIGHT}"          ▄▄▀▄░░░█─▀─▀─█░░░▄▀▄▄\n";
+printf ${CYAN_LIGHT}"          ░░░░▀▄▒▒▒▒▒▒▒▒▒▄▀░░░░\n";
+printf ${CYAN_LIGHT}"          ░░░░░█────▀────█░░░░░\n";
+printf ${CYAN_LIGHT}"          ░░░░░█────▀────█░░░░░\n\n";
+printf ${CYAN_LIGHT}"          ★★★★彡ＴＯＲＲＥＳ彡★★★★\n";
                                                                                                                                                          
   printf "            \033[1;33m        © Torres - https://www.emersontorres.com.br \n";
   printf "${NC}";
